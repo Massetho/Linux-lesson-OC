04f1bff6dfb32fd8ec7e6275c8a0d9eb4441ac38
Massetho <quentin_thomasset@hotmail.fr>
2017-02-25 00:48:21 +0100

    Final commission for the exercise

--- langstat.sh
+++ langstat.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-#Cette commande s'utilise ainsi : "./langstat fichier [lettre]"
+#Cette commande s'utilise ainsi : "./langstat.sh fichier [chaine]"
+#Sans le paramètre "[chaine]" la commande remplit la mission 1.
+#Avec le paramètre "[chaine]" la commande remplit la mission 2.
 
-
-#Gestion du paramètre principal
-if [ -z $1 ] || [ ! -f $1 ];
+#Vérification de la présence et de la validité du paramètre "fichier"
+if [ -z $1 ] || [ ! -f $1 ] ;
 then
-        echo 'Veuillez rentrer un nom de fichier valide à traiter';
-        echo 'Ex : ./langstat fichier_valide';
-        exit 1;
+	echo 'Veuillez rentrer un nom de fichier valide à traiter';
+	echo 'Ex : ./langstat fichier_valide';
+	exit 1;
 fi
 
-# Mission 1 : combien de fois est utilisée chaque lettre ?
+# Mission 1 : combien de fois est utilisée chaque lettre dans "fichier" ?
 if [ -z $2 ] ;
 then
-        for x in {A..Z};
-        do
-                echo "`grep -i -c $x $1` - $x";
-        done | sort -rn
+	for x in {A..Z};
+	do
+		echo "`grep -ic $x $1` - $x";
+	done | sort -rn
 
-#Mission 2 : combien de mots commencent par la  lettre ($2) ?
+# Mission 2 : combien de mots commencent par "[chaine]" dans "fichier" ?
 else
-        echo "`grep -i -c -E ^$2 $1` mots commencent par $2";
+	echo "`grep -icE ^$2 $1` mots commencent par $2";
 fi
